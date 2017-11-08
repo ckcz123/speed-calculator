9f1d968ffe76067a1f41e9fc577f3ce95fb52dd0
oc <ckcz123@126.com>
2017-11-08 15:33:09 +0800

    First commit

---
+++ speed-calculator/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace speed_calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_load = new System.Windows.Forms.Button();
+            this.hero_speed = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mode1 = new System.Windows.Forms.RadioButton();
+            this.mode2 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel_all = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(379, 16);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(97, 23);
+            this.btn_add.TabIndex = 100;
+            this.btn_add.Text = "添加新怪物";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(482, 16);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(111, 23);
+            this.btn_save.TabIndex = 101;
+            this.btn_save.Text = "保存怪物数据";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // btn_load
+            // 
+            this.btn_load.Location = new System.Drawing.Point(599, 16);
+            this.btn_load.Name = "btn_load";
+            this.btn_load.Size = new System.Drawing.Size(119, 23);
+            this.btn_load.TabIndex = 102;
+            this.btn_load.Text = "读取怪物数据";
+            this.btn_load.UseVisualStyleBackColor = true;
+            this.btn_load.Click += new System.EventHandler(this.btn_load_Click);
+            // 
+            // hero_speed
+            // 
+            this.hero_speed.Location = new System.Drawing.Point(87, 18);
+            this.hero_speed.Name = "hero_speed";
+            this.hero_speed.Size = new System.Drawing.Size(57, 21);
+            this.hero_speed.TabIndex = 1;
+            this.hero_speed.Text = "1.0";
+            this.hero_speed.TextChanged += new System.EventHandler(this.hero_speed_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "当前攻速";
+            // 
+            // mode1
+            // 
+            this.mode1.AutoSize = true;
+            this.mode1.Checked = true;
+            this.mode1.Location = new System.Drawing.Point(180, 20);
+            this.mode1.Name = "mode1";
+            this.mode1.Size = new System.Drawing.Size(71, 16);
+            this.mode1.TabIndex = 2;
+            this.mode1.TabStop = true;
+            this.mode1.Text = "普通模式";
+            this.mode1.UseVisualStyleBackColor = true;
+            // 
+            // mode2
+            // 
+            this.mode2.AutoSize = true;
+            this.mode2.Location = new System.Drawing.Point(258, 20);
+            this.mode2.Name = "mode2";
+            this.mode2.Size = new System.Drawing.Size(95, 16);
+            this.mode2.TabIndex = 3;
+            this.mode2.Text = "永不复还模式";
+            this.mode2.UseVisualStyleBackColor = true;
+            this.mode2.CheckedChanged += new System.EventHandler(this.mode1_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 103;
+            this.label2.Text = "怪物名称";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(109, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 104;
+            this.label3.Text = "怪物攻速";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(178, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 105;
+            this.label4.Text = "优先权";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(232, 64);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(149, 12);
+            this.label5.TabIndex = 106;
+            this.label5.Text = "战斗回合（只显示前20个）";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(652, 64);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 12);
+            this.label6.TabIndex = 107;
+            this.label6.Text = "操作";
+            // 
+            // panel_all
+            // 
+            this.panel_all.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel_all.AutoSize = true;
+            this.panel_all.Location = new System.Drawing.Point(12, 84);
+            this.panel_all.Name = "panel_all";
+            this.panel_all.Size = new System.Drawing.Size(719, 144);
+            this.panel_all.TabIndex = 108;
+            // 
+            // label7
+            // 
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(28, 231);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 12);
+            this.label7.TabIndex = 109;
+            this.label7.Text = "说明：";
+            // 
+            // label8
+            // 
+            this.label8.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(30, 254);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(575, 12);
+            this.label8.TabIndex = 110;
+            this.label8.Text = "1. 速度计算方法：例如我方速度为8，怪物速度为7，则每隔1/8秒我方行动一次，每隔1/7秒怪物行动一次。";
+            // 
+            // label9
+            // 
+            this.label9.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(30, 277);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(539, 12);
+            this.label9.TabIndex = 111;
+            this.label9.Text = "2. 优先权：若遇到同一时刻双方同时行动，则由拥有优先权的一方先行动。默认是我方拥有优先权。";
+            // 
+            // label10
+            // 
+            this.label10.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(30, 300);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(599, 12);
+            this.label10.TabIndex = 112;
+            this.label10.Text = "3. 永不复还模式下，将以我方攻速的平方根计算。例如攻速335%，应在框中输入3.35，实际攻速值约为1.8303。";
+            // 
+            // label11
+            // 
+            this.label11.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(30, 323);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(455, 12);
+            this.label11.TabIndex = 113;
+            this.label11.Text = "4. 本计算器采用浮点数计算，对于大数据可能会有些许误差，如有错误，敬请谅解。";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(743, 351);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.panel_all);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.mode2);
+            this.Controls.Add(this.mode1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.hero_speed);
+            this.Controls.Add(this.btn_load);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btn_add);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "速度计算器 By 艾之葵";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_load;
+        private System.Windows.Forms.TextBox hero_speed;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton mode1;
+        private System.Windows.Forms.RadioButton mode2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel_all;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+
+        private string lastDirectory, lastFilename;
+    }
+}
+
---
+++ speed-calculator/Form1.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace speed_calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lastDirectory = "";
+            lastFilename = "";
+        }
+
+        private void calculate(object sender = null, EventArgs e = null)
+        {
+            try
+            {
+                calculate(((Control)sender).Parent);
+            }
+            catch (Exception)
+            {
+                foreach (Control control in panel_all.Controls)
+                {
+                    calculate(control);
+                }
+            }
+        }
+
+        private void calculate(Control control)
+        {
+
+            if (!(control is Panel)) throw new Exception();
+
+            try
+            {
+                double speed = Convert.ToDouble(hero_speed.Text);
+                if (mode2.Checked)
+                {
+                    speed = Math.Sqrt(speed);
+                }
+
+                double mspeed = Convert.ToDouble(control.Controls.Find("monster_speed", true)[0].Text);
+                bool priority = ((CheckBox)control.Controls.Find("monster_priority", true)[0]).Checked;
+
+                if (speed < 1e-8 || mspeed < 1e-8) return;
+                speed = 1.0 / speed; mspeed = 1.0 / mspeed;
+
+                Queue<double> me = new Queue<double>(), monster = new Queue<double>();
+                List<int> ans = new List<int>();
+                for (int i = 1; i <= 30; i++)
+                {
+                    me.Enqueue(i * speed);
+                    monster.Enqueue(i * mspeed);
+                }
+                for (int i = 1; i <= 20; i++)
+                {
+                    double x = me.Peek(), y = monster.Peek();
+
+                    if (x < y - 1e-8)
+                    {
+                        ans.Add(0);
+                        me.Dequeue();
+                    }
+                    else if (y < x - 1e-8)
+                    {
+                        ans.Add(1);
+                        monster.Dequeue();
+                    }
+                    else
+                    {
+                        if (priority)
+                        {
+                            ans.Add(1);
+                            monster.Dequeue();
+                        }
+                        else
+                        {
+                            ans.Add(0);
+                            me.Dequeue();
+                        }
+                    }
+                }
+                draw(ans, (PictureBox)control.Controls.Find("picture", true)[0]);
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+
+        private void remove(object sender, EventArgs args)
+        {
+            if (panel_all.Controls.Count == 1) return;
+            try
+            {
+                Control parent = ((Control)sender).Parent;
+                int index = panel_all.Controls.IndexOf(parent);
+                if (index == -1) return;
+                remove(index);
+            }
+            catch (Exception) { }
+        }
+
+        private void remove(int index)
+        {
+            try
+            {
+                Control control = panel_all.Controls[index];
+                panel_all.Controls.RemoveAt(index);
+                control.Dispose();
+                for (int i = index; i < panel_all.Controls.Count; i++)
+                {
+                    Point location = panel_all.Controls[i].Location;
+                    location.Y -= 30;
+                    panel_all.Controls[i].Location = location;
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void draw(List<int> list, PictureBox picture)
+        {
+            Bitmap bitmap = new Bitmap(400, 20);
+            Bitmap me = new Bitmap(20, 20);
+            Graphics graphic = Graphics.FromImage(me);
+            graphic.FillRectangle(Brushes.MediumSeaGreen, 0, 0, 20, 20);
+            graphic.DrawImage(Properties.Resources.me, 0,0);
+            Bitmap monster = new Bitmap(20, 20);
+            graphic = Graphics.FromImage(monster);
+            graphic.FillRectangle(Brushes.Tomato, 0, 0, 20, 20);
+            graphic.DrawImage(Properties.Resources.monster, 0, 0);
+
+            Graphics graphics = Graphics.FromImage(bitmap);
+            for (int i = 0; i < 20 && i < list.Count; i++)
+            {
+                graphics.DrawImage(list[i]==0?me:monster, 20 * i, 0);
+            }
+            picture.Image = bitmap;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btn_add_Click(null, null);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen pen = new Pen(Color.Black);
+            pen.DashStyle = DashStyle.Solid;
+            Graphics graphics = CreateGraphics();
+            graphics.DrawLine(pen, 10, 50, ((Control)sender).Width - 32, 50);
+            pen.Dispose();
+            graphics.Dispose();
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            int cnt = panel_all.Controls.Count;
+
+            Panel panel = new Panel();
+            panel.Size = new System.Drawing.Size(700, 37);
+            panel.Location = new System.Drawing.Point(10, 30 * cnt - 5);
+
+            TextBox tb_name = new TextBox();
+            tb_name.Location = new System.Drawing.Point(8, 8);
+            tb_name.Size = new System.Drawing.Size(72, 20);
+            tb_name.Text = "怪物" + (cnt + 1);
+            tb_name.Name = "monster_name";
+            tb_name.TabIndex = 5 * cnt + 4;
+            panel.Controls.Add(tb_name);
+
+            TextBox tb_speed = new TextBox();
+            tb_speed.Location = new System.Drawing.Point(90, 8);
+            tb_speed.Size = new System.Drawing.Size(53, 20);
+            tb_speed.Text = "1.0";
+            tb_speed.TextChanged += calculate;
+            tb_speed.Name = "monster_speed";
+            tb_speed.TabIndex = 5 * cnt + 5;
+            panel.Controls.Add(tb_speed);
+
+            CheckBox cb_priority = new CheckBox();
+            cb_priority.Location = new System.Drawing.Point(169, 8);
+            cb_priority.Size = new System.Drawing.Size(20, 20);
+            cb_priority.Name = "monster_priority";
+            cb_priority.CheckedChanged += calculate;
+            cb_priority.TabIndex = 5 * cnt + 6;
+            panel.Controls.Add(cb_priority);
+
+            PictureBox picture = new PictureBox();
+            picture.Location = new System.Drawing.Point(212, 8);
+            picture.Size = new System.Drawing.Size(400, 20);
+            picture.Name = "picture";
+            picture.TabIndex = 5 * cnt + 7;
+            picture.BackColor = System.Drawing.Color.LightGray;
+            panel.Controls.Add(picture);
+
+
+            Button btn = new Button();
+            btn.Text = "删除";
+            btn.Location = new System.Drawing.Point(631, 8);
+            btn.Size = new System.Drawing.Size(47, 20);
+            panel.Controls.Add(btn);
+            btn.Click += remove;
+
+            panel_all.Controls.Add(panel);
+            calculate(panel);
+
+            tb_name.Focus();
+        }
+
+        private void hero_speed_TextChanged(object sender, EventArgs e)
+        {
+            calculate();
+        }
+
+        private void mode1_CheckedChanged(object sender, EventArgs e)
+        {
+            calculate();
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            if (string.IsNullOrEmpty(lastDirectory) || string.IsNullOrEmpty(lastFilename))
+            {
+                dialog.FileName = "save";
+                dialog.InitialDirectory = Directory.GetCurrentDirectory();
+            }
+            else
+            {
+                dialog.FileName = lastFilename;
+                dialog.InitialDirectory = lastDirectory;
+            }
+            dialog.Filter = "怪物攻速数据(*.monspeed)|*.monspeed";
+            dialog.AddExtension = true;
+            dialog.DefaultExt = ".monspeed";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string text = hero_speed.Text + " " + (mode1.Checked?0:1) + "\n";
+                int cnt = panel_all.Controls.Count;
+                text += cnt + "\n";
+                foreach (Control control in panel_all.Controls)
+                {
+                    text += control.Controls.Find("monster_name", true)[0].Text + " " +
+                            control.Controls.Find("monster_speed", true)[0].Text + " " +
+                            (((CheckBox)control.Controls.Find("monster_priority", true)[0]).Checked?1:0) + "\n";
+                }
+                File.WriteAllText(dialog.FileName, text);
+                lastDirectory = Path.GetDirectoryName(dialog.FileName);
+                lastFilename = Path.GetFileName(dialog.FileName);
+
+                MessageBox.Show("文件保存至 " + dialog.FileName + "。", "保存成功！");
+            }
+        }
+
+        private void btn_load_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "怪物攻速数据(*.monspeed)|*.monspeed";
+            // dialog.RestoreDirectory = true;
+            if (string.IsNullOrEmpty(lastDirectory) || string.IsNullOrEmpty(lastFilename))
+            {
+                dialog.InitialDirectory = Directory.GetCurrentDirectory();
+            }
+            else
+            {
+                dialog.InitialDirectory = lastDirectory;
+                dialog.FileName = lastFilename;
+            }
+            dialog.AddExtension = true;
+            dialog.DefaultExt = ".monspeed";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string[] text = File.ReadAllLines(dialog.FileName);
+                string[] hero = text[0].Split(" ".ToCharArray());
+                hero_speed.Text = hero[0];
+                // textbox_hero_def.Text = atkdef[1];
+                bool x = (Convert.ToInt32(hero[1]) == 0 ? (mode1.Checked = true) : (mode2.Checked = true));
+                int cnt = Convert.ToInt32(text[1]);
+                while (panel_all.Controls.Count < cnt) btn_add_Click(null,null);
+                while (panel_all.Controls.Count > cnt) remove(panel_all.Controls.Count - 1);
+                for (int i = 0; i < cnt; i++)
+                {
+                    string[] strings = text[2 + i].Split(" ".ToCharArray());
+                    Control control = panel_all.Controls[i];
+                    control.Controls.Find("monster_name", true)[0].Text = strings[0];
+                    control.Controls.Find("monster_speed", true)[0].Text = strings[1];
+                    ((CheckBox)control.Controls.Find("monster_priority", true)[0]).Checked = Convert.ToInt32(strings[2])!=0;
+                }
+                calculate();
+
+                lastDirectory = Path.GetDirectoryName(dialog.FileName);
+                lastFilename = Path.GetFileName(dialog.FileName);
+
+            }
+        }
+    }
+}
